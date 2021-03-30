5cb8df3f502b9c9d003b7df4aa15bc7ee2b7c1df
Jeremy <jlam198@my.bcit.ca>
2021-03-29 22:12:48 -0700

    Added bullet point instructions in app.js for notes app functions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-/*         */
+/*
+function to switch between dark and light mode
+- user clicks button
+- if background is light, change to dark
+- if background is dark, change to light
+*/
+
+/*  
+function to add a new note      
+- user clicks button
+- textarea and save / cancel buttons appears
+- user clicks save to save note to an array
+- user clicks cancel to remove note taking textarea and buttons
+*/
+
+/* 
+function to open and close side menu
+- user clicks button
+- menu is opened if close
+- menu is closed if open
+*/
